aec4129dff76ba0f49513ecac1c5c725d4d3be51
leechaeyeon321 <leechaeyeon321@gmail.com>
2022-11-19 11:54:06 +0900

    feat: homework

---
+++ javascript/variable.js
@@ -0,0 +1,15 @@
+// let name = 'Chaeyeon Lee'
+// console.log(name);
+
+// name = "william"
+// console.log(name);
+
+// const name = "chaeyeon Lee"
+// console.log(name)
+
+// name = "william"
+// const는 상수
+
+console.log(10)
+let age = 37
+console.log(age)
