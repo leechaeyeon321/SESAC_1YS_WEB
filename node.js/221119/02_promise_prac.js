73d0b90a52bfff9845e627186b5ae0dba72decac
leechaeyeon321 <leechaeyeon321@gmail.com>
2022-11-19 17:33:21 +0900

    feat: promise

--- node.js/221119/02_promise_prac.js
+++ node.js/221119/02_promise_prac.js
@@ -23,8 +23,8 @@ login('chaeyeon', '1234', function(user){
             console.log(msg);
         });
     });
-}); */
-
+}); 
+위에 내용을 프로미스로 바꾸기*/
 function login(id, pw) {
     return new Promise( function ( resolve, reject ) {
         setTimeout( function() {
@@ -33,7 +33,6 @@ function login(id, pw) {
         }, 3000);
     })
 }
-
 function getVideo(id) {
     return new Promise(function(resolve, reject){
         setTimeout(function(){
@@ -49,7 +48,6 @@ function getDetail(video) {
         }, 1000);
     });
 }
-
 login( 'kim', '1234' )
     .then( function(user){
         return getVideo(user);
@@ -61,27 +59,3 @@ login( 'kim', '1234' )
     .then( function(msg){
         console.log( "msg : ", msg );
     });
-/* function getVideo(id){
-    return new Promise (function (resolve, reject){
-        setTimeout(function(){
-            resolve(id);
-        }, 3000);
-    })
-}
-getVideo('kim').then(
-    function(id){
-    console.log(id + "의 비디오 리스트")
-    }
-)
-function getDetail(video){
-    return new Promise (function (resolve, reject){
-        setTimeout(function(){
-            resolve(video);
-        }, 3000);
-    })
-}
-getVideo('아바타').then(
-    function(video){
-        console.log("비디오 제목:" + video)
-    }
-) */
